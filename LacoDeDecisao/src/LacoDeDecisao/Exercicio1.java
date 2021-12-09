d50bf620a4067d5b6404e86f09a57986f53fbd0f
nycolas mendes <nycolascouto1@gmail.com>
2021-12-09 15:01:00 -0300

    Laços de Decisão

---
+++ LacoDeDecisao/src/LacoDeDecisao/Exercicio1.java
@@ -0,0 +1,27 @@
+package LacoDeDecisao;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		
+		int [] numeros = new int[4];
+		int cont = 0;
+		
+		@SuppressWarnings("resource")
+		Scanner nmr = new Scanner(System.in);
+		
+		for(int i = 1; i < numeros.length; i++){ 
+			System.out.println("Informe o numero " + i + ": ");
+			numeros[i] = nmr.nextInt();
+			
+			if(numeros[i] > cont) {
+				cont = numeros[i];
+			}
+		}
+		nmr.close();
+		System.out.println("O mair numero digitado foi: " + cont);
+	}
+
+}
